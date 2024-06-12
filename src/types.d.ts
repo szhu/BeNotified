4141ee8d529125a4825010f8ff2eea40307a131f
Sean Zhu <interestinglythere@gmail.com>
2024-06-13 03:23:02 +0800

    Add BeRealRegionId ts type

--- src/types.d.ts
+++ src/types.d.ts
@@ -3,13 +3,10 @@ declare const React: typeof import("react");
 // eslint-disable-next-line @typescript-eslint/naming-convention
 declare const ReactDOM: typeof import("react-dom/client");
 
+type BerealRegionId = "us-central" | "europe-west" | "asia-west" | "asia-east";
+
 interface CheckBerealResponse {
-  regions: {
-    "us-central": CheckBerealRegion;
-    "europe-west": CheckBerealRegion;
-    "asia-west": CheckBerealRegion;
-    "asia-east": CheckBerealRegion;
-  };
+  regions: Record<BerealRegionId, CheckBerealRegion>;
   now: {
     ts: number;
     utc: string;
